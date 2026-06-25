e4541ae158e64ca1b693ee8f1f5dd598a61cc82b
salvarez1121 <36012393+salvarez1121@users.noreply.github.com>
2026-06-25 16:23:39 -0500

    Delete quarkus-ai-integration directory

--- quarkus-ai-integration/input-documents/quarkus-jakarta-data/src/main/java/org/acme/BookingCannotBeCancelledException.java
+++
@@ -1,8 +0,0 @@
-package org.acme;
-
-public class BookingCannotBeCancelledException extends RuntimeException {
-
-    public BookingCannotBeCancelledException(String bookingNumber) {
-        super("Booking " + bookingNumber + " cannot be canceled");
-    }
-}
